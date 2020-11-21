a38e353838c65dcf7b5af466988e54f5f93d50dc
Andria <ashoniaa@gmail.com>
2020-11-21 20:16:39 +0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,92 +2,82 @@ let checkedService = "";
 let checkedInnerService = "";
 
 function removePopup() {
-    $(".popup-item").remove();
-    setTimeout(function (){
-    	location.reload()
-    }, 1000)
+  $(".popup-item").remove();
+  setTimeout(function () {
+    location.reload();
+  }, 1000);
 }
 
 //Enable/Desable confirm btn
 function enableDisable() {
-    if (checkedInnerService.length > 0 && checkedService.length > 0 && $("#locationChecked").val() === "true") {
-        $(".confirm-btn").prop("disabled", false)
-    } else {
-
-        $(".confirm-btn").prop("disabled", true)
-    }
+  if (
+    checkedInnerService.length > 0 &&
+    checkedService.length > 0 &&
+    $("#locationChecked").val() === "true"
+  ) {
+    $(".confirm-btn").prop("disabled", false);
+  } else {
+    $(".confirm-btn").prop("disabled", true);
+  }
 }
 
-$(function() {
+$(function () {
+  //Service chekers
+  $(".service-check").on("change", function () {
+    $(".serviceInner-check").prop("checked", false);
+    if ($(this).is(":checked")) {
+      $(".sub-btn-wrapper").slideUp();
+      $(".service-check").prop("checked", false);
 
+      checkedService = $(this).parent().find(".label").text().trim();
+      $(this).prop("checked", true);
+      $(this).parent().parent().find(".sub-btn-wrapper").slideDown();
+    } else {
+      $(this).prop("checked", false);
+      $(this).parent().parent().find(".sub-btn-wrapper").slideUp();
+      checkedService = "";
+    }
+    enableDisable();
+  });
 
-	//Service chekers
-    $(".service-check").on("change", function() {
+  //Service sub checkers
+  $(".serviceInner-check").on("change", function () {
+    if ($(this).is(":checked")) {
+      $(".serviceInner-check").prop("checked", false);
 
-        $(".serviceInner-check").prop("checked", false);
-        if ($(this).is(":checked")) {
+      $(this).prop("checked", true);
+      checkedInnerService = $(this).parent().find(".value").text().trim();
+    } else {
+      $(this).prop("checked", false);
+      checkedInnerService = "";
+    }
+    enableDisable();
+  });
 
-            $(".sub-btn-wrapper").slideUp();
-            $(".service-check").prop("checked", false);
+  //Location checkers
+  $(".location-check").on("change", function () {
+    if ($(this).is(":checked")) {
+      $(".location-check").prop("checked", false);
+      $(".location-item__body").slideUp();
+      $(".location-item").removeClass("active");
+      $("#locationChecked").val("true");
 
+      $(this).parent().next().slideDown();
+      $(this).parent().parent().addClass("active");
+      $(this).prop("checked", true);
+    } else {
+      $(this).prop("checked", false);
+      $("#locationChecked").val("false");
+      $(this).parent().next().slideUp();
+      $(this).parent().parent().removeClass("active");
+    }
+    enableDisable();
+  });
 
+  //Message popup
 
-            checkedService = $(this).parent().find(".label").text().trim();
-            $(this).prop("checked", true);
-            $(this).parent().parent().find(".sub-btn-wrapper").slideDown();
-
-        } else {
-
-            $(this).prop("checked", false);
-            $(this).parent().parent().find(".sub-btn-wrapper").slideUp();
-            checkedService = "";
-
-        }
-        enableDisable()
-    });
-
-
-    //Service sub checkers
-    $(".serviceInner-check").on("change", function() {
-        if ($(this).is(":checked")) {
-            $(".serviceInner-check").prop("checked", false);
-
-            $(this).prop("checked", true);
-            checkedInnerService = $(this).parent().find(".value").text().trim();
-        } else {
-            $(this).prop("checked", false);
-            checkedInnerService = ""
-        }
-        enableDisable()
-    });
-
-
-    //Location checkers
-    $(".location-check").on("change", function() {
-        if ($(this).is(":checked")) {
-            $(".location-check").prop("checked", false);
-            $(".location-item__body").slideUp();
-            $(".location-item").removeClass("active");
-            $("#locationChecked").val("true")
-
-
-            $(this).parent().next().slideDown();
-            $(this).parent().parent().addClass("active");
-            $(this).prop("checked", true);
-        } else {
-            $(this).prop("checked", false);
-            $("#locationChecked").val("false")
-            $(this).parent().next().slideUp();
-            $(this).parent().parent().removeClass("active");
-        }
-        enableDisable()
-    });
-
-
-    //Message popup
-
-    $(".confirm-btn").on("click", function() {
-        $("body").append(`<div class="popup-item">
+  $(".confirm-btn").on("click", function () {
+    $("body").append(`<div class="popup-item">
 							<div class="popup-body">
 								<h3 id="messageContent">
 									Congratulations, you've successfully submitted ${checkedInnerService} ${checkedService} request
@@ -98,5 +88,23 @@ $(function() {
 						</div>
 					</div>
 			`);
-    }); 
+  });
+
+  //Menu
+  $(".app-burger-menu__btn").on("click", function () {
+    $(this).toggleClass("active");
+    $(".fullscreen-menu").toggleClass("active");
+
+    TweenMax.staggerFrom(
+      ".fullscreen-menu .nav-menu li",
+      0.75,
+      {
+        opacity: 0,
+        yPercent: 105,
+        ease: Power3.easeOut,
+      },
+      0.05,
+      0.35
+    );
+  });
 });
